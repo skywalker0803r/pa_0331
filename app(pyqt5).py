5a527b8450578d206c97be96c9b27f29203caabc
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2020-04-30 15:45:17 +0800

    Add files via upload

--- app(pyqt5).py
+++ app(pyqt5).py
@@ -1,43 +1,58 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import (QWidget, QGridLayout,QPushButton, QApplication)
 import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+from utils import *
+import joblib
 
+# load api object
+api = joblib.load('./model/api.pkl')
+
+# window
 class Window(QWidget):
 	def __init__(self):
 		super().__init__()
 		self.setWindowTitle("PA廠計算機")
-		self.setGeometry(50,50,350,350)
+		self.setGeometry(50,50,350*2,350*2)
 		self.UI()
 
 	def UI(self):
-		#================================
-		self.answer = QLabel("",self)
-		self.answer.resize(200,100)
-		self.answer.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-		self.answer.move(80,200)
-		#================================
-		self.nameTextBox = QLineEdit(self)
-		self.nameTextBox.setPlaceholderText('set_point')
-		self.nameTextBox.move(120,50)
-		#==============================
-		self.passTextBox = QLineEdit(self)
-		self.passTextBox.setPlaceholderText('feed')
-		self.passTextBox.move(120,80)
-		#==============================
+		# answer area
+		self.answer = QtWidgets.QTableView(self)
+		self.answer.resize(800,800)
+		
+		# input area
+		self.TextBox = QLineEdit(self)
+		self.TextBox.setPlaceholderText('set_point')
+		
+		# button
 		button = QPushButton("calculate",self)
-		button.move(120,110)
 		button.clicked.connect(self.getValues)
-		#==============================
+		
+		# GridLayout
+		self.gridLayout = QGridLayout()
+		self.gridLayout.addWidget(self.TextBox,0,1)
+		self.gridLayout.addWidget(button,1,1)
+		self.gridLayout.addWidget(self.answer,2,1)
+		self.setLayout(self.gridLayout)
 		self.show()
 
 	def getValues(self):
-		name = self.nameTextBox.text()
-		password = self.passTextBox.text()
-		result = 'control factor1:'+str(float(name) + float(password))
-		result = result +'\n'+ result
-		self.answer.setText(result)
+		set_point = float(self.TextBox.text())
+		raw_advice = api.get_advice(set_point)
+		advice = api.pretty_advice(raw_advice)
+		output = api.get_critic_output(raw_advice)
+		model = PandasModel(advice)
+		self.answer.setModel(model)
 
-# exec
-App = QApplication(sys.argv)
-window = Window()
-sys.exit(App.exec_())
+# entry point
+def main():
+	App = QApplication(sys.argv)
+	window = Window()
+	sys.exit(App.exec_())
+
+if __name__ == "__main__":
+	main()
 
